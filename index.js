4f5d5796ebb404b218f05262188666297a20a363
Yash Garg <yash431garg@gmail.com>
2021-06-25 18:25:25 +0530

    v1 favicon

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
+var favicon = require("serve-favicon");
 require("dotenv").config();
 
 const app = express();
 app.use(express.static(path.join(__dirname, "assets")));
+
+app.use(favicon(path.join(__dirname + "/assets/img/favicon.ico")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
