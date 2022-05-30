e437265a9b3bb57e947b10f1e294264458163111
Yash Garg <yash431garg@gmail.com>
2022-05-30 17:37:06 +0530

    changes in duration to 1000

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 const sr = ScrollReveal({
   origin: "top",
   distance: "80px",
-  duration: 500,
+  duration: 1000,
   reset: true,
 });
 
